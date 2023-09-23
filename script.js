4b4c9c9adc7373af5093f1a7f2adbfa9f597d62c
Ironakatina <116155572+Ironakatina33@users.noreply.github.com>
2023-09-23 13:38:55 +0200

    money and money /s system added

--- script.js
+++ script.js
@@ -1,76 +1,44 @@
-/* Les évènements */
 
-// Comment on écoute un évènement ?
-// 1 : Qui ? div, button, a, p ... élément HTML
-// 2 : Quoi ? Quel type d'évènement
-
-
-
-const back = document.querySelector('.bodyb')
-const button = document.querySelector('.my-button')
-
-// Qui.addEventListener(Quoi, function)
-button.addEventListener('click', function(){
-    //le code de ma fonction
-    console.log('click')
-    back.classList.remove("brouge");
-    back.classList.remove("bjaune");
-    back.classList.remove("bbleu");
-    back.classList.add("bblanc");
-
-
-})
+money = 0
+const makemoney = document.querySelector('.make-money')
+const textMoney = document.querySelector('.money')
+const upgrade1 = document.querySelector('.upgrade-1')
+const textautomoney = document.querySelector('.automoney')
+mult = 1
+automoney = 0
 
 
 
 
 
 
+  
+  // Qui.addEventListener(Quoi, function)
+    makemoney.addEventListener('click', function(){
+        //le code de ma fonction
+        console.log('click')
+        money = money + mult
+
+        textMoney.textContent = money + " $"
+
+    })
+
+    upgrade1.addEventListener('click', function(){
+        if (money >= 100)
+            upgrade1.style.display = 'none';
+            money = money - 100
+            textMoney.textContent = money + " $"
+            mult = mult + 10
+            automoney = automoney + 1 
+
+    })
+
+    setInterval(automoneyfunc, 1000)
 
 
-/* EXERCICE 4 - 15 minutes
 
-Créer 3 boutons et pour chacun faire un écouter d'évènement 
-qui change la couleur de ma page lorsque je clique dessus.
-
-*/
-
-const Redbutton = document.querySelector('.Rbutton')
-
-// Qui.addEventListener(Quoi, function)
-Redbutton.addEventListener('click', function(){
-    //le code de ma fonction
-    console.log('Rouge')
-    back.classList.remove("bblanc");
-    back.classList.remove("bjaune");
-    back.classList.remove("bbleu");
-    back.classList.add("brouge");
-
-})
-
-const Bluebutton = document.querySelector('.Bbutton')
-
-// Qui.addEventListener(Quoi, function)
-Bluebutton.addEventListener('click', function(){
-    //le code de ma fonction
-    console.log('Bleu')
-    back.classList.remove("bblanc");
-    back.classList.remove("bjaune");
-    back.classList.remove("brouge");
-    back.classList.add("bbleu");
-
-})
-
-const Yelbutton = document.querySelector('.Ybutton')
-
-// Qui.addEventListener(Quoi, function)
-Yelbutton.addEventListener('click', function(){
-    //le code de ma fonction
-    console.log('Jaune')
-    back.classList.remove("bblanc");
-    back.classList.remove("brouge");
-    back.classList.remove("bbleu");
-    back.classList.add("bjaune");
-
-})
-
+    function automoneyfunc(){
+        money = money + automoney
+        textautomoney.textContent = automoney + " /s"
+        textMoney.textContent = money + " $"
+    }
